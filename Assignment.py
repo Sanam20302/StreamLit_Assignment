ebc7205a45cfe0b000341d560f34f26adbfe4364
Sanam20302 <76154182+Sanam20302@users.noreply.github.com>
2024-04-18 19:50:32 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 st.title("Graded Assignment Week - 8")
+st.header("Find the largest among the 3 given numbers")
 num1 = st.number_input('Enter the first number')
 num2 = st.number_input('Enter the second number')
 num3 = st.number_input('Enter the third number')
-st.write(max([num1,num2,num3]))
+st.write("The largest among the three is : ",max([num1,num2,num3]))
